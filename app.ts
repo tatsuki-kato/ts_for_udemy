0693271a91f50dddcc5aae491a03333a1658f9d7
tatsuki-kato <tkiucky0712@gmail.com>
2022-12-13 06:45:05 +0900

    string型の配列定義

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+// } 
+const person = {
     name: 'yota',
     age: 40,
+    hobbies: ['Sports', 'Cooking']
 }
+
+let favoriteActivities: string[];
+
 
 console.log(person.name);
