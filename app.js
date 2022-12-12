b0982241247612206c88b83508fae7fab6b995c1
MoyuKa <tkiucky0712@gmail.com>
2022-12-12 21:38:18 +0900

    app系のファイルコミット

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("ここにコードを追加します。");
+function add(n1, n2, shoResult) {
+    // if(t/ypeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('入力正しくない')
+    // }
+    if (shoResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var prinResult = true;
+var result = add(number1, number2, prinResult);
