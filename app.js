f08a3dca1e115c1d4e877258c981f3d4cc1bae31
tatsuki-kato <tkiucky0712@gmail.com>
2022-12-13 07:01:14 +0900

    enum型の定義

--- app.js
+++ app.js
@@ -2,14 +2,34 @@
 //     name: string;
 //     age: number;
 // } 
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'yota',
+//     age: 40,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author'],
+// }
+// let favoriteActivities: string[];
+// favoriteActivities = ['Sports'];
+// for(const hobby of person.hobbies){
+//     console.log(hobby.toUpperCase());
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'yota',
     age: 40,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+    console.log("正解");
 }
