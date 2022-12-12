6f222b835c775dc4d6f7b0f406fabdd1a32fbf9a
tatsuki-kato <tkiucky0712@gmail.com>
2022-12-13 06:52:16 +0900

    tupple型の定義

--- app.ts
+++ app.ts
@@ -2,14 +2,20 @@
 //     name: string;
 //     age: number;
 // } 
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'yota',
     age: 40,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
 }
 
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
+// let favoriteActivities: string[];
+// favoriteActivities = ['Sports'];
 
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase());
