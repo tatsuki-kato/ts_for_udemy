5d8ef8a23925c300103d0a6d2e67b75a76844a72
tatsuki-kato <tkiucky0712@gmail.com>
2022-12-13 07:04:12 +0900

    enum型の練習

--- app.js
+++ app.js
@@ -20,9 +20,9 @@
 // }
 var Role;
 (function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 7] = "AUTHOR";
 })(Role || (Role = {}));
 var person = {
     name: 'yota',
